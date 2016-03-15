b8a5f4d67f20e82eca7a6ff649d2dbee6059d298
Juliette Lonij <juliette.lonij@kb.nl>
2016-03-15 18:28:59 +0100

    Add accuracy.py

---
+++ accuracy.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.insert(0, '../dac')
+
+import disambiguation
+import json
+import urllib
+
+
+with open('users/test/art.json') as data_file:
+    data = json.load(data_file)
+
+instance_count = 0
+candidate_count = 0
+correct_pred = 0
+pos_label = 0
+
+for i in data['instances']:
+
+    print('Reviewing instance', i['url'], instance_count)
+
+    # Check if instance has been properly labeled
+    if not i['link'] == '':
+
+        print 'Link found, getting Solr results', i['link']
+
+        linker = disambiguation.EntityLinker()
+        result = linker.link(i['url'], i['ne_string'].encode('utf-8'))[0]
+        solr_response = linker.to_link.solr_response
+        print result
+
+        if 'link' in result:
+            print 'Prediction', result['link']
+        else:
+            print 'No prediction'
+
+        # Check if DBpedia canadidates have been retrieved
+        if hasattr(solr_response, 'numFound') and solr_response.numFound > 0:
+
+            index = 0
+
+            for r in solr_response:
+                print 'solr_link', r['id'][1:-1]
+
+                # Label 1
+                if r['id'][1:-1] == i['link']:
+                    pos_label += 1
+                    if 'link' in result and result['link'] == r['id'][1:-1]:
+                        correct_pred += 1
+                        print('correct prediction')
+                    else:
+                        print 'wrong prediction'
+
+                # Label 0
+                else:
+                    if 'link' in result:
+                        if result['link'] != r['id'][1:-1]:
+                            correct_pred += 1
+                            print('correct prediction')
+                        else:
+                            print 'wrong prediction'
+                    else:
+                        correct_pred += 1
+                        print('correct prediction')
+
+                candidate_count += 1
+
+            index += 1
+
+    instance_count += 1
+
+accuracy = float(correct_pred) / float(candidate_count)
+
+print('number of candidates', candidate_count)
+print('number of positive labels', pos_label)
+print('number of correct predictions', correct_pred)
+print('accuracy', accuracy)
+
