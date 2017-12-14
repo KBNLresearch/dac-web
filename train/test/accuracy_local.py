818ffe52b5aa9a9a1a826a5a4ea5234e1d50cd4c
Juliette Lonij <juliette.lonij@kb.nl>
2017-12-14 16:59:26 +0100

    Update training set

--- train/test/accuracy_local.py
+++ train/test/accuracy_local.py
@@ -29,9 +29,9 @@ import dac
 with open('../../users/test-20/art.json') as fh:
     data = json.load(fh)
 
-linker = dac.EntityLinker(model='bnn', debug=True)
+linker = dac.EntityLinker(model='svm', debug=True)
 
-with open('results-bnn-60.csv', 'w') as fh:
+with open('results-svm-62.csv', 'w') as fh:
 
     keys = ['id', 'entity', 'links', 'prediction', 'correct']
 
